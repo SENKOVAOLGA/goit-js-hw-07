ccd2241ccbf422a74be06c2827010ab5d9f6df3c
SENKOVAOLGA <senkovaom320@ukr.net>
2023-10-21 22:16:34 +0300

    summary

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+  const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                  <a class="gallery__link" href="${original}">
+                     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+                  </a>
+              </li>`
+     })
+     .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+gallery.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    if (target.classList.contains("gallery__image")) {
+        const originalImgUrl = target.dataset.source;
+        const modal = basicLightbox.create(`<img src="${originalImgUrl} width="800" height="600"`);
+        modal.show();
+    }
+}
+     
+// function imageClick(event) {
+//      event.preventDefault();
+//      const target = event.target;
+//      if (target.classList.contains('gallery__image')) {
+//          const originalImageUrl = target.dataset.source;
+//          const modal = basicLightbox.create(`
+//          <img src="${originalImageUrl}" width="800" height="600">
+//          `);
+//          modal.show();
+//      }
+// }
