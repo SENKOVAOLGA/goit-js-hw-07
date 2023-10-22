3409e6b932dc5c8c835515e26c9022cc5fc91cbb
SENKOVAOLGA <senkovaom320@ukr.net>
2023-10-23 00:56:35 +0300

    createGalleryMarking

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = document.querySelector(".gallery");
-  const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-                  <a class="gallery__link" href="${original}">
-                     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-                  </a>
-              </li>`
-     })
-     .join("");
-gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-gallery.addEventListener("click", handlerClick);
-function handlerClick(evt) {
-    evt.preventDefault();
-    const target = evt.target;
-    if (target.classList.contains("gallery__image")) {
-        const originalImgUrl = target.dataset.source;
-        const modal = basicLightbox.create(`<img src="${originalImageUrl} width="800" height="600"`);
-        modal.show();
-    }
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarking(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered items
+function createGalleryMarking(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
 }
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
+}
+
+
  
