c8bd10f0657854f5d208a0935d42a51eb8a07ce0
SENKOVAOLGA <senkovaom320@ukr.net>
2023-10-23 01:14:33 +0300

    app new code

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,21 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
-const gallery = document.querySelector(".gallery");
-  const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-                  <a class="gallery__link" href="${original}">
-                     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-                  </a>
-              </li>`
-     })
-      .join("");
-gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-let lightBox = new simplelightBox('.gallery a', {
-    captions: true,
-    captionDelay: 250,
-    fadeSpeed: 250,
-    captionSelector: "img",
-    captionData: 'alt',
-    captionPosition: 'bottom',
-});
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryMarking(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+ function createGalleryMarking(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+ });
